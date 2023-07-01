ac32347f6b28b1661ca45a9c9182f3dc87a6b134
btwl <1253102853@qq.com>
2023-07-01 13:29:18 +0800

    update

--- update_to_github.sh
+++ update_to_github.sh
@@ -13,4 +13,8 @@ echo ----------done-------------------
 
 
 # todo:
+# 1.
 # the commit msg should input from .sh para use "msgs..." to send to .sh file 
+# 2.
+# how to make echo ----------status------------- this when output to terminal have special color .
+# i want it to be more obvius infomation. we could say highlight it ,or the summary
