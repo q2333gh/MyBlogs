9ab5c00a0bf0af90159a72fd9af4096548d6b0b6
btwl <1253102853@qq.com>
2023-07-01 16:07:33 +0800

    Update convertDocx2md.sh

--- convertDocx2md.sh
+++ convertDocx2md.sh
@@ -12,3 +12,8 @@ pandoc --extract-media ./myMediaFolder CN.docx -o output.md
 # convert todo things:
 # the output image too big. dont have to this big .
 # goal is somehow compress it . so just enough to display things.
+
+同时，打建好博客以后发布一篇博客，为博客系统的metadata， so cool 。 内容是如何搭建一个这样的博客，并个性化，和发布第一篇博客，以及一些自动化脚本 抽象了多个操作，变成了declarative的编程
+
+16行的文本是在github上面操作的，因此和本地产生了冲突
+如何merge这两个。并同步对方？
